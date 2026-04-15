6ffb6a5e2efc60f8f4d443bb10ca3d408720bd28
Nayan Jyoti <nayan2024cswork@gmail.com>
2026-04-15 22:52:49 +0530

    Fix vite base path issue

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server :{ host:true},
-  base:process.env.VITE_API_BASE_URL || "/satsangTez",
- 
+  plugins: [react(), tailwindcss()],
+  server: { host: true },
+  base: "/",   // ✅ IMPORTANT FIX
 })
